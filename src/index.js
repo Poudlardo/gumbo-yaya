f4a61eba1604d48da2265fc545e8d246e037a222
Poudlardo <55178305+Poudlardo@users.noreply.github.com>
2023-09-18 17:39:37 +0200

    render html elements dynamiically

--- src/index.js
+++ src/index.js
@@ -1,1 +1,5 @@
-console.log('This is my console.log statement from /src/index.js HAHA!')
+import * as module from './initialmodule'
+
+console.log("statement always banging");
+
+module.initalModule();
---
+++ src/initialmodule.js
@@ -0,0 +1,24 @@
+export function initalModule() {
+    
+const contentDiv = document.querySelector('#content');
+const heading = document.createElement('h1');
+heading.textContent = "Resaurant Page";
+contentDiv.appendChild(heading);
+
+const topImage = document.createElement('img');
+topImage.classList.add('top-image');
+topImage.src = '../src/assets/2631198.jpg';
+topImage.alt = 'Image of Restaurant';
+contentDiv.appendChild(topImage);
+
+const para1 = document.createElement('p');
+para1.classList.add('landing-page-copy');
+para1.textContent = 'The best rooftop experience you will taste in Paris';
+contentDiv.appendChild(para1);
+
+const para2 = document.createElement('p');
+para2.classList.add('landing-page-copy');
+para2.textContent = "Take a deep dive in french finest cuisine, by the greatests Michelin-awarded chef";
+contentDiv.appendChild(para2);
+
+}
