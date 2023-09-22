511828d3712451b7512f54f1cc0661d67e3d5f87
Poudlardo <55178305+Poudlardo@users.noreply.github.com>
2023-09-22 23:45:19 +0200

    add menu hours and prices list

--- src/initialmodule.js
+++ src/initialmodule.js
@@ -1,3 +1,5 @@
+import yelpImg from './assets/Yelp.png';
+
 export function initalModule() {
 
 const contentDiv = document.querySelector('#content');
@@ -22,19 +24,21 @@ subTitle2.innerHTML = "S O U T H E R N  K I T C H E N  ★  C H I C K E N�
 divSubTitle.appendChild(subTitle);
 divSubTitle.appendChild(subTitle2);
 contentDiv.appendChild(divSubTitle);
+
 // Animation mis en pause à chaque passage de souris
-
-
 const redFacade = document.createElement('div');
 redFacade.classList.add('red-facade');
 contentDiv.appendChild(redFacade);
+
+const hoursGraff = document.createElement('p');
+hoursGraff.classList.add('hours-graff');
+hoursGraff.innerHTML = '<h2>MONDAY<br>to FRIDAY</h2>12:00AM-2:15PM<br>7:30PM-10:30PM<br><br><h2>SATURDAY</h2>12:00AM-2:15PM<br>7:30PM-10:30PM<br><br><h2>SUNDAY</h2>12:00AM-3:00PM';
+redFacade.appendChild(hoursGraff);
 
 const doorWrapper = document.createElement('div');
 doorWrapper.classList.add('door-wrapper');
 redFacade.appendChild(doorWrapper);
 
-
-// derrière la porte
 // left door
 const leftDoor = document.createElement('div');
 leftDoor.classList.add('left-door','door');
@@ -44,13 +48,20 @@ const rightDoor = document.createElement('div');
 rightDoor.classList.add('right-door','door');
 doorWrapper.appendChild(rightDoor);
 // haut de porte
-const topDoor = document.createElement('div')
+const topDoor = document.createElement('div');
 topDoor.classList.add('top-door');
 doorWrapper.appendChild(topDoor);
 
 const orangeWindow = document.createElement('div');
 orangeWindow.classList.add('orange-window');
 redFacade.appendChild(orangeWindow);
+
+const relief3 = document.createElement('div');
+const relief4 = document.createElement('div');
+relief3.classList.add('vitre');
+relief4.classList.add('vitre');
+leftDoor.appendChild(relief3);
+rightDoor.appendChild(relief4);
 
 //shapes on the door
 const relief = document.createElement('div');
@@ -60,10 +71,32 @@ relief2.classList.add('door-relief');
 leftDoor.appendChild(relief2);
 rightDoor.appendChild(relief);
 
-// derrière la vitre
-const insideShop = document.createElement('img');
-insideShop.src = './assets/2023-09-14.jpg';
-orangeWindow.appendChild(insideShop);
+const menuGraff = document.createElement('div');
+menuGraff.classList.add('menu-graff');
+const meals = document.createElement('p');
+meals.classList.add('meals');
+meals.innerHTML = 'Chicken & Waffles<br>Waffle burger<br>Filet & waffles<br>Chicken burger<br>2 pieces w/side<br>2 filets w/ side<br> 3 pieces w/ 2 sides<br> Veggie bowl';
+menuGraff.appendChild(meals)
 
+const prices = document.createElement('p');
+prices.classList.add('prices');
+prices.innerHTML = '11 €<br>12 €<br>13 €<br>12 €<br>11 €<br>13 €<br>15 €<br>10 €';
+menuGraff.appendChild(prices)
+redFacade.appendChild(menuGraff);
 
+//logos on the window
+
+orangeWindow.appendChild(yelpImg);
+
+const tripadvisor = document.createElement('img');
+tripadvisor.src = './assets/tripadvisor.png'
+orangeWindow.appendChild(tripadvisor);
+
+const timeout = document.createElement('img');
+timeout.src = './assets/timeout.png';
+orangeWindow.appendChild(timeout);
+
+const streetSign = document.createElement('img');
+streetSign.src = './assets/streetsign.png';
+topDoor.appendChild(streetSign);
 }
