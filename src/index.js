7562d10734df4352aef0972d00c885dfc8f7cef2
Poudlardo <55178305+Poudlardo@users.noreply.github.com>
2023-09-22 12:02:05 +0200

    add animations on the page

--- src/index.js
+++ src/index.js
@@ -1,27 +1,6 @@
 import { initalModule } from './initialmodule';
-/*import { menu } from './menu.js';
-import { info } from './info.js';
-import { about } from './about.js';
-import { jobs } from './jobs.js';*/
 import css from './style.css'
 
 console.log("statement always banging");
 
 initalModule();
-
-/*let getToNewPage = (function() {
-    let homeTab = document.querySelector('.home');
-    homeTab.addEventListener('click', initalModule);
-
-    let menuTab = document.querySelector('.menu');
-    menuTab.addEventListener('click', menu);
-
-    let aboutTab = document.querySelector('.about');
-    aboutTab.addEventListener('click', about)
-
-    let infoTab = document.querySelector('.info');
-    infoTab.addEventListener('click', info)
-
-    let jobsTab = document.querySelector('.jobs');
-    jobsTab.addEventListener('click', jobs)
-})()*/
--- src/initialmodule.js
+++ src/initialmodule.js
@@ -2,29 +2,68 @@ export function initalModule() {
 
 const contentDiv = document.querySelector('#content');
 contentDiv.replaceChildren();
-
+// La div du Titre 
 const heading = document.createElement('h1');
+const divHeading = document.createElement('div');
+divHeading.classList.add('div-heading');
 heading.textContent = "GUMBO YAYA";
-contentDiv.appendChild(heading);
+divHeading.appendChild(heading);
+contentDiv.appendChild(divHeading);
 
-const subTitle = document.createElement('h2');
-subTitle.textContent = "SOUTHERN KITCHEN";
-contentDiv.appendChild(subTitle);
+// La Div du Sous-Titre
+const subTitle = document.createElement('span');
+const divSubTitle = document.createElement('div');
+const subTitle2 = document.createElement('span');
+divSubTitle.classList.add('parent-subtitle');
+subTitle.classList.add('child-subtitle');
+subTitle2.classList.add('child-subtitle','child-subtitle2');
+subTitle.innerHTML = "S O U T H E R N  K I T C H E N  ★  C H I C K E N  A N D  W A F F L E S  ★";
+subTitle2.innerHTML = "S O U T H E R N  K I T C H E N  ★  C H I C K E N  A N D  W A F F L E S  ★";
+divSubTitle.appendChild(subTitle);
+divSubTitle.appendChild(subTitle2);
+contentDiv.appendChild(divSubTitle);
+// Animation mis en pause à chaque passage de souris
 
-const topImage = document.createElement('img');
-topImage.classList.add('top-image');
-topImage.src = '../src/assets/2631198.jpg';
-topImage.alt = 'Image of Restaurant';
-contentDiv.appendChild(topImage);
 
-const para1 = document.createElement('p');
-para1.classList.add('landing-page-copy');
-para1.textContent = 'The best rooftop experience you will taste in Paris';
-contentDiv.appendChild(para1);
+const redFacade = document.createElement('div');
+redFacade.classList.add('red-facade');
+contentDiv.appendChild(redFacade);
 
-const para2 = document.createElement('p');
-para2.classList.add('landing-page-copy');
-para2.textContent = "Take a deep dive in french finest cuisine, by the greatests Michelin-awarded chef";
-contentDiv.appendChild(para2);
+const doorWrapper = document.createElement('div');
+doorWrapper.classList.add('door-wrapper');
+redFacade.appendChild(doorWrapper);
+
+
+// derrière la porte
+// left door
+const leftDoor = document.createElement('div');
+leftDoor.classList.add('left-door','door');
+doorWrapper.appendChild(leftDoor);
+// right door
+const rightDoor = document.createElement('div');
+rightDoor.classList.add('right-door','door');
+doorWrapper.appendChild(rightDoor);
+// haut de porte
+const topDoor = document.createElement('div')
+topDoor.classList.add('top-door');
+doorWrapper.appendChild(topDoor);
+
+const orangeWindow = document.createElement('div');
+orangeWindow.classList.add('orange-window');
+redFacade.appendChild(orangeWindow);
+
+//shapes on the door
+const relief = document.createElement('div');
+const relief2 = document.createElement('div');
+relief.classList.add('door-relief');
+relief2.classList.add('door-relief');
+leftDoor.appendChild(relief2);
+rightDoor.appendChild(relief);
+
+// derrière la vitre
+const insideShop = document.createElement('img');
+insideShop.src = './assets/2023-09-14.jpg';
+orangeWindow.appendChild(insideShop);
+
 
 }
